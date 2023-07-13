c9c0c87380786ca27d183d45cae566cbae05b836
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-07-13 06:04:40 +0000

    first commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require rails-ujs
 
-
-import Rails from "@rails/ujs"
+import Rails from "rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
