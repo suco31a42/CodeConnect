b9a3b8a8ff24939dfe73352480c8de9f1f33a743
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-07-21 08:28:50 +0000

    [fix]master.key

--- config/routes.rb
+++ config/routes.rb
@@ -9,29 +9,29 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     root to: 'end_users#index'
-    resources :end_users, only: %i[show edit update] 
+    resources :end_users, only: %i[show edit update]
     resources :posts, only: %i[index show destroy] do
       resources :post_comments, only: %i[destroy]
     end
   end
-  
+
   scope module: 'public' do
     root to: 'homes#top'
-  
+
     resources :end_users, only: %i[show edit update destroy] do
       member do
         get 'confirm', 'follows', 'followers'
         patch 'withdraw'
       end
       resource :relationships, only: %i[create destroy]
-  
+
     end
-    resources :notifications, only: %i[index] 
+    resources :notifications, only: %i[index]
     delete :notifications, to: "notifications#destroy_all"
-  
+
     resources :posts, only: %i[index create show edit update destroy] do
       member do
         delete :delete_image
