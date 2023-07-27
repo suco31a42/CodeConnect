00b6ce496455b7bc69ae7657c935fb3b124ebd1a
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-07-27 06:06:23 +0000

    [fix]kaminari

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,11 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
+    if params[:oldest]
+      @posts = Post.all.order(created_at: :asc).page(params[:page]).per(10)
+    else
+      @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
+    end
   end
 
   def show
