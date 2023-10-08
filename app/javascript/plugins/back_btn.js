37545cc8493365cd651baf968a3bab289100e294
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-10-08 10:58:11 +0000

    [update]back_btn

--- app/javascript/plugins/back_btn.js
+++ app/javascript/plugins/back_btn.js
@@ -1,8 +1,20 @@
-$(function () {
-  $('#back a').on('click',function(event){
+$(function() {
+  var back = $('#back');
+  // 普段は消す
+  back.hide();
+  $(window).scroll(function () {
+    // スクロールの値が500以上でフェードイン
+    if ($(this).scrollTop() > 500) {
+      back.fadeIn();
+    } else {
+      back.fadeOut();
+    }
+  });
+
+  back.click(function (){
     $('body, html').animate({
       scrollTop:0
-    },800);
-    event.preventDefault();
+    }, 800);
+    return false;
   });
 });
